94e287cf8714ff4c044735566c3b4dc6e5963939
alejandro-piguave <alejandro.piguave.illescas@gmail.com>
2022-08-15 00:35:02 +0200

    Add token verification code to origin branch

--- src/main/kotlin/com/example/tinderclonebackend/security/provider/FirebaseAuthenticationProvider.kt
+++ src/main/kotlin/com/example/tinderclonebackend/security/provider/FirebaseAuthenticationProvider.kt
@@ -1,6 +1,9 @@
 package com.example.tinderclonebackend.security.provider
 
 import com.example.tinderclonebackend.model.FirebaseToken
+import com.google.firebase.auth.AuthErrorCode
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthException
 import org.springframework.security.authentication.AuthenticationProvider
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
@@ -8,10 +11,21 @@ import org.springframework.stereotype.Component
 @Component
 class FirebaseAuthenticationProvider: AuthenticationProvider {
     override fun authenticate(authentication: Authentication?): Authentication? {
-        val tokenString = authentication!!.principal.toString()
-        return if(tokenString == "123456")
+        val tokenString = authentication?.principal.toString()
+
+        //Code for testing
+        /*return if(tokenString == "123456")
             FirebaseToken("alex_id")
-        else null
+        else null*/
+
+        return try {
+            val checkRevoked = true
+            val decodedToken = FirebaseAuth.getInstance().verifyIdToken(tokenString, checkRevoked)
+            val uid = decodedToken.uid
+            FirebaseToken(uid)
+        } catch (e: FirebaseAuthException) {
+            null
+        }
     }
 
     override fun supports(authentication: Class<*>): Boolean {
